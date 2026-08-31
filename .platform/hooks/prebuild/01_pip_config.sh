58a1da2153fd181a7eaa7ffb75daf3e29ed27699
sgl <shivukumargl13@gmail.com>
2026-08-31 15:54:00 +0530

    Fix XGBoost deployment dependency

--- .platform/hooks/prebuild/01_pip_config.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir -p /opt/pip-tmp
-chmod 1777 /opt/pip-tmp
--- .platform/hooks/preinit/01_mount_tmp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p /opt/eb-tmp
-chmod 1777 /opt/eb-tmp
-
-mount --bind /opt/eb-tmp /tmp
-chmod 1777 /tmp
