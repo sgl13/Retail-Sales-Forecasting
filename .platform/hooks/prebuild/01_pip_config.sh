dcd0b8681371eea0946e0b7487b6d1714c5bdf11
sgl <shivukumargl13@gmail.com>
2026-08-31 15:32:03 +0530

    Use root disk for pip temporary files

--- .platform/hooks/prebuild/01_pip_config.sh
+++ .platform/hooks/prebuild/01_pip_config.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-mkdir -p /var/app/pip-tmp
-chmod 777 /var/app/pip-tmp
-
-export TMPDIR=/var/app/pip-tmp
-export TMP=/var/app/pip-tmp
-export TEMP=/var/app/pip-tmp
-
 export PIP_NO_CACHE_DIR=1
 export PIP_DISABLE_PIP_VERSION_CHECK=1
+export TMPDIR=/opt/pip-tmp
+export PIP_TMPDIR=/opt/pip-tmp
